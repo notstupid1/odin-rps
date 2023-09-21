d457eed3c2a4f1765649bbf84d66239f14e7a437
notstupid1 <143924946+notstupid1@users.noreply.github.com>
2023-09-21 11:28:23 +0530

    Added play logic

--- rps.js
+++ rps.js
@@ -1,8 +1,33 @@
 function getComputerChoice() {
-    choices = ["Rock", "Paper", "Scissor"];
-    rand = Math.floor((Math.random() *10)) % 3;
+    let choices = ["Rock", "Paper", "Scissor"];
+    let rand = Math.floor((Math.random() *10)) % 3;
     return choices[rand];
 }
 
-comp_choice = getComputerChoice();
-console.log(comp_choice);
+function playRound (comp_choice, player_choice) {
+    switch (comp_choice) {
+        case "Rock":
+            if(player_choice === "Paper")
+                return `You Win! ${comp_choice} beaten by ${player_choice}`;
+            else if(player_choice === "Scissor")
+                return `You Lose! ${comp_choice} beats ${player_choice}`;
+            else return `You Tie! ${comp_choice} vs ${player_choice}`;
+        case "Paper":
+            if(player_choice === "Scissor")
+                return `You Win! ${comp_choice} beaten by ${player_choice}`;
+            else if(player_choice === "Rock")
+                return `You Lose! ${comp_choice} beats ${player_choice}`;
+            else return `You Tie! ${comp_choice} vs ${player_choice}`;
+        case "Scissor":
+            if(player_choice === "Rock")
+                return `You Win! ${comp_choice} beaten by ${player_choice}`;
+            else if(player_choice === "Paper")
+                return `You Lose! ${comp_choice} beats ${player_choice}`;
+            else return `You Tie! ${comp_choice} vs ${player_choice}`;
+    }
+}
+
+let comp_choice = getComputerChoice();
+let player_choice = "Rock";
+let win_status = playRound(comp_choice, player_choice);
+console.log(win_status);
